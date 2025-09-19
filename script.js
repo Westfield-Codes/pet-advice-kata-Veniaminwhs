8e14899420e9ab20ad2a6ec5872d769d26468a14
Veniaminwhs <veniamin208879@student-schoolsofwestfield.org>
2025-09-19 12:11:27 -0400

    Not done

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-/* Pet Advice Kata */
-
-/* Var, Prompt, Confirm, Alert, Compound Conditional */
-
-// Create pet variable, set equal to empty quotes ("")
-// Ask if they are allergic, store in a boolean variable with OK/Cancel
-// If they are allergic, ask if they want a cute pet, store in a boolean variable with OK/Cancel
-// If they are not allergic, ask if they want to be outdoors with their pet.  Store it in a boolean with OK/Cancel.
-// If allergic and cute, pet =  bird
-// Else if allergic and not cute,  pet =  tarantula
-// Else if not allergic and outdoors,  pet =  dog
-// Else if not allergic and indoors,  pet =  cat 
-// Display the type of pet: You would do well with a pet
-// Ask for a pet name for the pet starting with the same letter as the pet starts with
-// Display petname the pet says hi. 
+var pet =("");
+var allergic = confirm(" are you allergic ? ");
+if (allergic == true) {
+    var cute = confirm("cute [ok] creepy [cancel]? ")
+    if (cute == true) pet = "bird";
+    else pet = "snake";
+}
+else {
+var outside = confirm("outside [ok] inside [cancel] pet ?")
+} 
+alert("try a"+ pet)
